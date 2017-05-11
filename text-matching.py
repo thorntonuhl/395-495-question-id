2e74ed128fbdad6ec2a0f9775d33c91266eb215d
Christopher Mark <cmark1017@gmail.com>
2017-05-10 20:30:13 -0500

    modified term-matching

--- text-matching.py
+++ text-matching.py
@@ -2,7 +2,8 @@ import nltk
 from nltk.corpus import stopwords
 import re
 
-# how htis should work...
+
+# how this should work...
 # input question as string
 # extract key words from string
 # search for keywords in 'database'
@@ -12,18 +13,11 @@ import re
 # nltk.download('punkt')
 # nltk.download('averaged_perceptron_tagger')
 # nltk.download('stopwords')
-# pos tag question
-# make copy of question
-# remove stop words
-# extract nouns / compound nouns/ verbs/ adjectives?
-#   put in array
-# iteratively regex.search each term on database
-# return list of narrowed DB results
-# somehow narrow it down more...
 
 # text = nltk.word_tokenize("What should I do if the head injury is bleeding heavily?\n")
 # print  nltk.pos_tag(text)
 
+# perform search on a categorized database... as opposed to general DB
 stop_words = set(stopwords.words('english'))
 def format_db(file):
     db = []
@@ -37,23 +31,42 @@ def find_match(question):
     text = nltk.word_tokenize(question)
     tagged_q = nltk.pos_tag(text)
     keywords = []
+    # extract keywords
     for tup in tagged_q: #JJ, NN, VB,
         word, pos = tup
         if 'JJ' in pos or 'NN' in pos or 'VB' in pos:
             if word not in stop_words: # to use or not to use stopwords???
                 keywords.append(word)
+
+    # print outputs
     print "TAGGED SENTENCE: {}".format(tagged_q)
     print "KEYWORDS: {}".format(keywords)
-    text = open('q_list.txt')
-    string = text.read()
-    text.close()
-    matches = []
-    for kw in keywords:
-        matches += re.findall("[^\n]*{0}[^?]*\\?".format(kw), string)
-    print "MATCHES: {}".format(matches)
+    # find matches in DB
+    with open('q_list.txt') as text:
+        string = text.read()
+        text.close()
+        matches = []
+        for kw in keywords:
+            # matches += '\n'.join(re.findall("[^\n]*{0}[^?]*\\?".format(kw), string))
+            matches += re.findall("[^\n]*{0}[^?]*\\?".format(kw), string)
+
+    matches = "\n".join(matches)
+    # print "MATCHES: {}".format(matches)
+
+    # # PHASE 2
+    keyword_len = len(keywords)
+    more_matches = []
+    if keyword_len > 1:
+        for i in range(0,keyword_len-1):
+            bigram = "{0} {1}".format(keywords[i],keywords[i+1])
+            more_matches += list(set(re.findall("[^\n]*{0}[^?]*\\?".format(bigram), matches)))
+        print more_matches
+        # if no matches, just search for BOTH keywords in each line...
+    else:
+        print matches.split("\n")
     return 0
 
-db = format_db('q_list.txt')
+# db = format_db('q_list.txt')
 def main():
     question = raw_input("enter question: ")
     find_match(question)
